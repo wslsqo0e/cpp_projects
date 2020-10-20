c5690fcacf1c260d4cc4a05ecdca75495e64084b
wslsqo0e <shenming23@126.com>
2020-10-20 16:38:29 +0800

    chage Aho-Corasick from file process to streaming process

--- Aho-Corasick/main.cc
+++ Aho-Corasick/main.cc
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <fstream>
+#include <iostream>
 #include "acism.h"
 
 static int actual = 0, details = 1;
@@ -12,7 +13,42 @@ static int on_match(int strnum, int textpos, MEMREF const *pattv)
   return 0;
 }
 
-int main(int argc, char *argv[])
+int main(int argc, char *argv[]) {
+  if (argc != 3) {
+    fprintf(stderr, "%s pattern_file [count]\ne.g. %s patts 2\n", argv[0], argv[0]);
+    return 1;
+  }
+
+  int count = atoi(argv[2]);
+  if (count < 0 || count > 50) {
+    fprintf(stderr, "count value is imappropriate: %d\n", count);
+  }
+  MEMBUF patt = chomp(read_file(argv[1]));
+
+  if (!patt.ptr) {
+    die("cannot read %s", argv[1]);
+  }
+
+  int npatts;
+  MEMREF *pattv = refsplit(patt.ptr, '\n', &npatts);
+
+  ACISM *psp = acism_create(pattv, npatts);
+
+  details = 0;
+
+  std::string line;
+  while (std::getline(std::cin, line)) {
+    actual = 0;
+    int state = 0;
+    MEMREF text = {line.c_str(), line.size()};
+    (void)acism_more(psp, text, (ACISM_ACTION*)on_match, pattv, &state);
+    if (actual > count) {
+      printf("%s\n", line.c_str());
+    }
+  }
+}
+
+int main_old(int argc, char *argv[])
 {
   if (argc < 2 || argc > 4) {
     fprintf(stderr, "%s pattern_file target_file [[-]expected]\ne.g. %s patts act.txt -5\n",
@@ -30,27 +66,15 @@ int main(int argc, char *argv[])
 
   double t = tick();
   ACISM *psp = acism_create(pattv, npatts);
-  printf("%p\n", psp);
   t = tick() - t;
 
   fprintf(stdout, "acism_create(pattv[%d]) compiled, in %.3f secs\n", npatts, t);
-
-  if (argc > 2) {
-    int state = 0;
-
-    details = 1;
-    std::ifstream ifs(argv[2]);
-    if (!ifs) {
-      die("cannot open %s", argv[2]);
-    }
-  }
 
   if (argc > 2) {
     int			state = 0;
 
     details = 0;
     std::ifstream ifs(argv[2]);
-    // printf("%s\n", argv[2]);
     if (!ifs) {
       die("cannot open %s", argv[2]);
     }
